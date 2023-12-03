a536e10d18fa346fe3088f3be59f6f4c2b407666
unknown <brianwagako@gmail.com>
2023-12-03 21:52:38 +0300

    marketing page

---
+++ app/(Marketing)/_components/footer.tsx
@@ -0,0 +1,23 @@
+import {Logo} from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export const Footer=()=>{
+    return(
+        <div className="fixed bottom-0 w-full p-4 border-t bg-slate-100">
+           <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+                <Logo />
+
+                <div className="space-x-4 md:w-auto flex items-center justify-between w-full">
+                    <Button size="sm" variant="ghost">
+                        privacy policy
+                    </Button>
+                    <Button size="sm" variant="ghost">
+                       Terms of service
+                    </Button>
+
+                </div>
+           </div>
+        </div>
+    ); 
+};
