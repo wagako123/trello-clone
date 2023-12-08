ba5a3bb75d7d87110fdf2da96a0af194d52bc1e2
unknown <brianwagako@gmail.com>
2023-12-08 10:46:49 +0300

    loading skeleton in the sidebar

--- app/(Platform)/(dashboard)/_components/nav-item.tsx
+++ app/(Platform)/(dashboard)/_components/nav-item.tsx
@@ -10,6 +10,7 @@ import {
     Settings
 }from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export type Organization ={
     id:string;
@@ -110,4 +111,15 @@ const onClick =(href:string) =>{
 
         </AccordionItem>
     )
+};
+
+NavItem.Skeleton = function SkeletonNavItem(){
+    return(
+        <div className="flex items-center">
+            <div className="w-10 h-10 relative shrink-0">
+                <Skeleton className="h-full w-full absolute"/>
+            </div>
+            <Skeleton className="h-10 w-full" />
+        </div>
+    );
 };
